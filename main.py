0aa844fc3b614b4c8700701bc8f486adad3533e9
Nix <nikolaslopes.dev@gmail.com>
2021-07-06 16:39:10 -0400

    add some stykles and break lines

--- main.py
+++ main.py
@@ -20,14 +20,28 @@ sleep(1)
 
 print("\t\t\t\t\t (👍≖‿‿≖)👍 👍(≖‿‿≖👍)\n\n")
 
-sleep(1)
+sleep(3)
 
-video_url = input("Please, past the video's url here: ")
+clearConsole()
+
+sleep(0.5)
+
+print("\n\n\t（っ＾▿＾）  ☜(ˆ▿ˆc)\n")
+video_url = input(f"Please, past the video's url here: ")
+
+print("\n")
 
 youtube = pytube.YouTube(video_url)
 
-print(f"Downloading video: | {youtube.title}")
+sleep(1)
 
-video = youtube.stream.first()
+print(f"\nDownloading video: {youtube.title}\n\n")
+
+sleep(3)
+
+
+video = youtube.streams.first()
 
 video.download()
+
+print("\n\n\t\t\t\t*** DOWNLOAD COMPLETED ***\n\n\t\t\t\t(っ◔◡◔)っ ❤ \t ❤(ˆ‿ˆԅ)\n\n\n")
