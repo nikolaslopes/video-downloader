b4b6ed955e97c544d3e5c4082a2ff152d17a4968
Nix <nikolaslopes.dev@gmail.com>
2021-07-06 16:01:19 -0400

    add clear console

--- main.py
+++ main.py
@@ -3,3 +3,13 @@ import os
 import pytube
 
 from time import sleep
+
+
+def clearConsole():
+    command = 'clear'
+    if os.name in ('nt', 'dos'):
+        command = 'cls'
+    os.system(command)
+
+
+clearConsole()
